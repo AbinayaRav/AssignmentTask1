7594153a0ff2150c32ccb02783f88f2bd5070d95
AbinayaRav <abirav26@gmail.com>
2020-11-09 01:10:14 -0500

    Changed Scanner method in PrintNumberFromUser.java file

--- src/assignmentPackage/PrintNumberFromUser.java
+++ src/assignmentPackage/PrintNumberFromUser.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class PrintNumberFromUser {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter a number: ");
-        int userData = scan.nextInt();
-        System.out.println("\nUser entered the below number: ");
+        System.out.println("Please enter data: ");
+        String userData = scan.nextLine();
+        System.out.println("\nUser entered the below data: ");
         System.out.println(userData);
     }
 }
